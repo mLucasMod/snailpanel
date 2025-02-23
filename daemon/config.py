3838e69280665e6f3c1cb6d3839ad55986f4b072
mLucasMod <lucasdacruz08@hotmail.com>
2025-02-24 00:17:59 +0100

    code cleanup

--- daemon/config.py
+++ daemon/config.py
@@ -1,4 +1,1 @@
 PID_FILE = '/tmp/daemon.pid'
-
-# import os
-# PID_FILE = os.path.join(os.getcwd(), 'daemon\\daemon.pid')
--- daemon/restart_daemon.py
+++
@@ -1,5 +0,0 @@
-import start_daemon, stop_daemon
-
-if __name__ == "__main__":
-    stop_daemon.run()
-    start_daemon.run()
