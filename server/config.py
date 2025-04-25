90512983b776a951a4115712580596b9efd3bd35
mLucasMod <lucasdacruz08@hotmail.com>
2025-04-25 22:21:58 +0200

    WIP backend config

--- server/config.py
+++ server/config.py
@@ -1,11 +1,29 @@
 import json
 import os
 
-def get_config(name: str):
-    config_path = os.path.join(os.path.dirname(__file__), "config/server.json")
+def get_api_config(name: str):
+    config_path = os.path.join(os.path.dirname(__file__), "config/api.json")
 
     if not os.path.exists(config_path):
         raise FileNotFoundError(f"File not found : {config_path}")
 
     with open(config_path, "r") as file:
         return json.load(file).get(name, {})
+
+def get_database_config():
+    config_path = os.path.join(os.path.dirname(__file__), "config/database.json")
+
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"File not found : {config_path}")
+
+    with open(config_path, "r") as file:
+        return json.load(file).get("database", {})
+
+def get_daemon_config():
+    config_path = os.path.join(os.path.dirname(__file__), "config/daemon.json")
+
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"File not found : {config_path}")
+
+    with open(config_path, "r") as file:
+        return json.load(file).get("daemon", {})
