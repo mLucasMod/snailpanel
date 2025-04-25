492c00e76fdf1649ec5d2952f9f958a348f4db78
mLucasMod <lucasdacruz08@hotmail.com>
2025-04-25 22:22:42 +0200

    WIP cleanup

--- server/api/app/users/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, id: int, nom: str):
-        self.id = id
-        self.nom = nom
-
-    def toJson(self) -> map:
-        return {"id": self.id, "nom": self.nom}
